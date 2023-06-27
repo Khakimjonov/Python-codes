f1e9ee95422396268cdc8e0e25066c28b96cd706
Olimjon2009 <137907794+Olimjon2009@users.noreply.github.com>
2023-06-27 21:22:07 +0500

    Add files via upload
    oy, va yilni hisoblash uchun dastur

---
+++ python code _2.py
@@ -0,0 +1,71 @@
+#1
+import os
+os.system("cls")
+while 1:
+  baho=int(input("Bahoni kiriting :  "))
+  if baho==5:
+     print("Alo ! - juda zo'r baho !")
+  elif baho==4:
+     print("yaxshi !Barakalla")
+  elif baho==3:
+     print("qoniqarli! - harakat qil !")
+  elif baho==2:
+     print("Qoniqarsiz ! yaxshiroq o'qi hurmatli foydalanuvchi !")
+  else:
+     print("Bunday baho turi mavjud emas ! -qaytadan urinib ko'ring !")
+     break
+
+
+
+#2
+#import os
+#os.system("cls")
+#while 1:
+    
+#  month = int(input("1 dan 12 gacha istalgan raqam  kiriting > :  "))
+#  if month==1:
+#    print("Yanvar - Qish faslining boshlanishi . Bu fasl menga juda yoqadi. sizgachi ? -")
+#  elif  month==2:
+#   print("Fevral - qishning tugashi")
+#  elif  month==3:
+#    print("Mart - Bahorning ilk oyi , fasllar kelinchagi bahor fasli")
+#  elif  month==4:
+#    print("Aprel - bahorning nafis kunlari va mehribon onajonimning tug'ilgan oylari: yana qiziq ma'lumot 20 aprel payg'ambarimiz Muhammad.s.a.v ning tavvalud kunlari ekan")
+#  elif  month==5:
+#    print("May - Bahorning ohirgi oyi")
+#  elif month==6:
+#    print("Iyun - hamma bolalar sevgan yozning 1-oyi! -chunki maktab yo'q ")
+#  elif month==7:
+#    print("Iyul - Yozning o'rtasi ")
+#  elif month==8:
+#    print("Avgust - Yozning oxiri")
+#  elif month==9:
+#    print("Sentyabr - Oltin kuz boshlanishi")
+#  elif month==10:
+ #   print("Oktyabr - kuz faslining o'rtasi")
+#  elif month==11:
+ #   print("Noyabr kuzning tugashi")
+#  elif month==12:
+ #   print("Dekabr - oppoq qish fasli kirib keldi")
+ # else:
+#    print("Noto'g'ri kiritish !  -  404 error number not found !!!")
+#   break
+  
+  
+  
+  
+  #3
+#import os
+#os.system("cls")
+#while 1:
+ #seasons = int(input("Select your   season >  "))
+# if seasons==1:
+ # print("Qish - ko'pincha insonlarning yoqtirgan fasli deb o'ylayman. Ayniqsa men qishni juda yaaxshi ko'raman .")
+ #elif seasons==2:
+ # print("Bahor - Bu fasllar kelinchagi , uyg'onish fasli ! o'zbek milliy bayramlarimiz ham bahor faslida .")
+ #elif seasons==3:
+ # print("Yoz - Bu bolalarning eng sevimli fasli. Chunki bu faslda bolalar suv havzalariga borishadi va ta'tilda turli oromgohlarga borishadi>")
+ #elif seasons==4:
+ # print("Kuz - tabiat olovranga burkangan fasl. bu faslda butun tabiat olovranga burkanadi.")
+ #else:
+ # break
